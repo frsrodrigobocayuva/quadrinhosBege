6b86d02b7acca92736d60c6031cb0b3d5f4b2dcc
Rodrigo Bocayuva <frs.rodrigobocayuva@gmail.com>
2023-11-04 13:50:35 -0300

    Completando header principal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import './App.css'
 import HeaderButton from './Components/HeaderButton/HeaderButton';
 import {AiOutlineSearch} from 'react-icons/ai'
+import {FaUserCircle} from 'react-icons/fa'
+import {MdOutlineFavoriteBorder} from 'react-icons/md'
+import {BsCart3} from 'react-icons/bs'
+import {PiBooksDuotone} from 'react-icons/pi'
 
 function App() {
   return (
     <div className="App">
       <div className='uiBar'>
+        <div>
+          <a href='www.link' className='logo'>
+            <PiBooksDuotone className='iconeTitulo'/>
+            <h1 className='titulo'>
+              Quadrinhos<br/>bege
+            </h1>
+          </a>
+        </div>
         <form className='formPesquisa'>
           <input type='text' className='inputPesquisa' placeholder='O que você deseja?'/>
           <button type='submit' className='botaoPesquisa'>
             <i><AiOutlineSearch id='iconePesquisa'/></i>
           </button>
         </form>
+        <div className='iconesUi'>
+          <a href='www.link'><FaUserCircle className='iconeUi'/></a>
+          <a href='www.link'><MdOutlineFavoriteBorder className='iconeUi'/></a>
+          <a href='www.link'><BsCart3 className='iconeUi'/></a>
+        </div>
       </div>
       <section className='header'>
         <div className='headerButtons'>
